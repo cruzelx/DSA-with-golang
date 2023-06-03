39a3957a62169cf67f5e38c76a6e775962d3660b
Alex Bhattarai <cruze.alex@gmail.com>
2023-06-03 13:09:58 +0545

    visual representation of tree

--- tree/checkIfBST/main.go
+++ tree/checkIfBST/main.go
@@ -1,21 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
 func main() {
 	tree := GenerateBST()
 	PrintTree(tree)
 
-	isBST := CheckIfBST(tree, math.MinInt, math.MaxInt)
-	fmt.Println("IS BST ? : ", isBST)
+	// isBST := CheckIfBST(tree, math.MinInt, math.MaxInt)
+	// fmt.Println("IS BST ? : ", isBST)
 
-	tree = GenerateNonBST()
-	PrintTree(tree)
+	// tree = GenerateNonBST()
+	// PrintTree(tree)
 
-	isBST = CheckIfBST(tree, math.MinInt, math.MaxInt)
-	fmt.Println("IS BST ? : ", isBST)
+	// isBST = CheckIfBST(tree, math.MinInt, math.MaxInt)
+	// fmt.Println("IS BST ? : ", isBST)
 
 }
